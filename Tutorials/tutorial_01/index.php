2f7fb5f4356c064b99df991643e2b2069a44c177
thuzar96 <thuzarlwin.zara96@gmail.com>
2021-12-15 21:45:28 +0630

    [main] table data for chess board

--- Tutorials/tutorial_01/index.php
+++ Tutorials/tutorial_01/index.php
@@ -4,29 +4,30 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
   <title>Document</title>
 </head>
 <body>
-<table width="270px" cellspacing="0px" cellpadding="0px" border="1px">
-   <!-- cell 270px wide (8 columns x 60px) -->
-      <?php
-      for($row=1;$row<=8;$row++)
-	  {
-          echo "<tr>";
-          for($col=1;$col<=8;$col++)
+<table class="content">
+  <?php
+    for($row=1;$row<=8;$row++)
+    {
+      echo "<tr>";
+      for($col=1;$col<=8;$col++)
 		  {
-          $total=$row+$col;
-          if($total%2==0)
-		  {
-          echo "<td height=30px width=30px bgcolor=#FFFFFF></td>";
-          }
-		  else
-		  {
+        $total=$row+$col;
+        if($total%2==0)
+		    {
+          echo "<td class='white'></td>";
+        }
+		    else
+        {
           echo "<td height=30px width=30px bgcolor=#000000></td>";
-          }
-          }
-          echo "</tr>";
+        }
+      }
+        echo "</tr>";
     }
-          ?>
+  ?>
+</table> <!-- /.table---->
 </body>
 </html>

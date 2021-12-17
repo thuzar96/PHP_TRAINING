a15f0427c23632bd89b08059673779c1d5fe539d
thuzar96 <thuzarlwin.zara96@gmail.com>
2021-12-17 12:35:43 +0630

    [main] Fixed Reviews in tutorial 4

--- Tutorials/tutorial_04/login.php
+++ Tutorials/tutorial_04/login.php
@@ -1,12 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Welcome</title>
-</head>
-<body>
-  <h1>Hello </h1>
-</body>
-</html>
+<?php
+ $uname="admin";
+ $pwd="admin";
+
+session_start();
+
+
+if(isset($_SESSION['uname'])){  
+  echo "<h1>Welcome ".$_SESSION['uname']."</h1>";
+  echo "<a href='logout.php'><input type=button value=LOG OUT name=logout></a>";
+}
+
+//function to check username and password
+else{
+  //if username and password is correct
+  if($_POST['uname']==$uname && $_POST['pwd']==$pwd){
+    $_SESSION['uname']==$uname;
+    echo"<script> location.href='welcome.php'</script>";
+  }
+  else
+  //if username and password is incorrect
+  {
+    echo"<script> alert('username or password incorrect!')</script>";
+    echo "<script>location.href='index.php'</script>";
+  }
+}
+?>

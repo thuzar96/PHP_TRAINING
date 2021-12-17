21081d0119719096106dfc42e21fae1b4334f7dc
thuzar96 <thuzarlwin.zara96@gmail.com>
2021-12-17 16:59:26 +0630

    [main] added tutorial 5

--- Tutorials/tutorial_05/index.php
+++ Tutorials/tutorial_05/index.php
@@ -2,25 +2,78 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="widt h=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Document</title>
 </head>
 
 <body>
-    <div>
-        <h2>Text</h2>
-        <?php
-        if ($file = fopen("sample.txt", "r")) {
-            while (!feof($file)) {
-                $line = fgets($file);
-                echo ("$line" . "<br>");
-            }
-            fclose($file);
+  <div class="text">
+    <h2>Text</h2>
+    <?php
+    if ($file = fopen("sample.txt", "r")) {
+      while (!feof($file)) {
+        $line = fgets($file);
+        echo ("$line" . "<br>");
+      }
+      fclose($file);
+    }
+    ?>
+  </div>
+  <div class="text">
+    <h2>Document File</h2>
+    <?php
+    if ($file = fopen("sample.doc", "r")) {
+      while (!feof($file)) {
+        $line = fgets($file);
+        echo ("$line" . "<br>");
+      }
+      fclose($file);
+    }
+    ?>
+  </div>
+  <div class="text">
+    <h2>CSV File</h2>
+    <?php
+    //How to create CSV file from array in php
+    $row = 1;
+    if (($file = fopen('sample.csv', 'r')) !== FALSE) {
+      while (($data = fgetcsv($file, 1000, ",")) !== FALSE) {
+        $num = count($data);
+        echo "<p>fields in line $row: <br /></p>\n";
+        $row++;
+        for ($c = 0; $c < $num; $c++) {
+          echo $data[$c] . "<br />\n";
         }
-        ?>
-    </div>
+      }
+      fclose($file);
+    }
+    ?>
+  </div>
+  <div class="text">
+    <h2>Excel File</h2>
+    <?php
+    //reading excel file
+    require_once 'xlsx.php';
+    if ($xlsx = SimpleXLSX::parse('sample.xlsx')) {
+      echo "<table><tbody>";
+      $i = 0;
+      foreach ($xlsx->rows() as $row) { 
+        if ($i == 0) {
+          echo "<tr><th>" . $row[0] . "</th></tr>";
+        } else {
+          echo "<tr><td>" . $row[0] . "</td></tr>";
+        }
+        $i++;
+      }
+      echo "</tbody></table>";
+    } else {
+      echo SimpleXLSX::parseError();
+    }
+    ?>
+  </div>
 </body>
 
 </html>

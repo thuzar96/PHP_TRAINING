6e04324baa39bd53d970961c566ed60eba19bb15
thuzar96 <thuzarlwin.zara96@gmail.com>
2021-12-17 13:13:29 +0630

    [main] Fixed Reviews in tutorial 4

--- Tutorials/tutorial_04/welcome.php
+++ Tutorials/tutorial_04/welcome.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Welcome</title>
 </head>
+
 <body>
   <h1>Hello </h1>
+  <a href="logout.php"><input type="button" value="Log Out"></a>
 </body>
+
 </html>

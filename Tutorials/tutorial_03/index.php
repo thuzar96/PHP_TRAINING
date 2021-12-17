909754989ba56c7fca2b0f59b49aa15e7432c9d0
thuzar96 <thuzarlwin.zara96@gmail.com>
2021-12-17 11:57:32 +0630

    [main] link css to php

--- Tutorials/tutorial_03/index.php
+++ Tutorials/tutorial_03/index.php
@@ -17,6 +17,8 @@ return 'Your Age: ' .$diff->y. 'Years, '.$diff->m.'Months, '.$diff->d.' Days';
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/style.css">
   <title>Calculating Date</title>
 </head>
 <body>

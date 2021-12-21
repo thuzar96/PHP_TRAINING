1fc62031f79848cf16de918dfa3a72c283cbae4c
thuzar96 <thuzarlwin.zara96@gmail.com>
2021-12-21 15:42:52 +0630

    [main] added tutorial_06

--- Tutorials/tutorial_06/index.php
+++ Tutorials/tutorial_06/index.php
@@ -2,32 +2,33 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 
 <body>
-  <form method="POST">
-    <input type="file" name="upload"><br>
-    <label for="folder">Folder Name</label><br>
-    <input type="text" name="folder"><br>
-    <input type="submit" name="img_submit">
+    <form action="" method="POST" enctype="multipart/form-data">
+        <input type="file" name="upload"><br>
+        <label for="folder">Folder Name</label><br>
+        <input type="text" name="folder"><br>
+        <input type="submit" name="submit" value="SAVE">
 
-  </form>
-  <?php
-  $folder = $_POST['folder'];
-  $img_name = $_FILES['img_upload']['name'];
-  $tmp_img_name = $_FILES['img_upload']['tmp_name'];
+    </form>
+    <?php
+    $folder = $_POST['folder'];
+    $img_name = $_FILES['upload']['name']; // to choose folder
+    $tmp_img_name = $_FILES['upload']['tmp_name'];// variable to upload folder or create folder via input
 
-  if (isset($_POST['img_submit'])) {
-    mkdir($folder);
-    move_uploaded_file($tmp_img_name, "$folder/$img_name");
-  }
+    //to save seleced folder into chosen folder
+    if (isset($_POST['submit'])) {
+        mkdir($folder);
+        move_uploaded_file($tmp_img_name, "$folder/$img_name");
+    }
 
 
-  ?>
+    ?>
 </body>
 
 </html>

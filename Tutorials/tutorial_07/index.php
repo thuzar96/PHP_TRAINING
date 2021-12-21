ab348dc88b04b74351263f302c27e8b575c05599
thuzar96 <thuzarlwin.zara96@gmail.com>
2021-12-21 14:07:28 +0630

    [main] qr code generate

--- Tutorials/tutorial_07/index.php
+++ Tutorials/tutorial_07/index.php
@@ -1,14 +1,34 @@
 <?php
-  
-// Include the qrlib file
-include 'phpqrcode/qrlib.php';
-  
-// $text variable has data for QR 
-$text = "GEEKS FOR GEEKS";
-  
-// QR Code generation using png()
-// When this function has only the
-// text parameter it directly
-// outputs QR in the browser
-QRcode::png($text);
+echo "Generating QR code";
+include('library/phpqrode/qrlib.php');
+    include('config.php');
+
+    // how to save PNG codes to server
+    
+    $tempDir = "qrcodes";
+    
+    $codeContents = 'This is my QR code';
+    
+    // we need to generate filename somehow, 
+    // with md5 or with database ID used to obtains $codeContents...
+    $fileName = '005_file_'.md5($codeContents).'.png';
+    
+    $pngAbsoluteFilePath = $tempDir.$fileName;
+    $urlRelativeFilePath = $tempDir.$fileName;
+    
+    // generating
+    if (!file_exists($pngAbsoluteFilePath)) {
+        QRcode::png($codeContents, $pngAbsoluteFilePath);
+        echo 'File generated!';
+        echo '<hr />';
+    } else {
+        echo 'File already generated! We can use this cached file to speed up site on common codes!';
+        echo '<hr />';
+    }
+    
+    echo 'Server PNG File: '.$pngAbsoluteFilePath;
+    echo '<hr />';
+    
+    // displaying
+    echo '<img src="'.$urlRelativeFilePath.'" />'
 ?>
